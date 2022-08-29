1e07c19262a27bf46c5a07797202f0d7e8a2a29b
ocsoares <casoares016@gmail.com>
2022-08-28 21:02:51 -0300

    change email nodemailer

--- src/scripts/nodemailer-script.ts
+++ src/scripts/nodemailer-script.ts
@@ -11,14 +11,14 @@ export const sendNodemailer = () => (req: Request, res: Response, next: NextFunc
         port: 465,
         secure: true, // True para 465 e False para TODO O RESTO de Portas !! <<
         auth: {
-            user: 'wqszp12@gmail.com', // Email que vai ENVIAR um email !! <<
+            user: 'nodevalidation12@gmail.com', // Email que vai ENVIAR um email !! <<
             pass: process.env.NODEMAILER_PASS // NESSE CASO, com Gmail, precisa CRIAR uma Senha PRÓPRIA para Programas de Terceiros !! <<
         },
         tls: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } as any : false
     });
 
     newTransport.sendMail({
-        from: 'Aleatorio <wqszp12@gmail.com>', // Nome do Email (ACHO QUE É OPCIONAL) + <Email> (Se NÃO tiver o Nome do Email, pode colocar o Email DIRETO sem os <> ) !! <<
+        from: 'Node Validation <nodevalidation12@gmail.com>', // Nome do Email (ACHO QUE É OPCIONAL) + <Email> (Se NÃO tiver o Nome do Email, pode colocar o Email DIRETO sem os <> ) !! <<
         to: email, // Se quiser, pode ser uma LISTA de Emails !! <<
         subject: 'Cadastro concluído com sucesso !', // Assunto do Email
         text: `Olá ${username}, seu cadastro foi realizado com sucesso, seja bem vindo(a) !` // Texto do Email (Também pode usar uma Propriedade chamada html !!) !! <<
